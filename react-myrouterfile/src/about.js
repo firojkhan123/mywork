7f7d36685417e9a7c2e0b3eacedc891306989458
Firoj Khan <firojkhan@Ayushis-MacBook-Air.local>
2019-03-06 16:30:24 +0530

    Add existing file

---
+++ react-myrouterfile/src/about.js
@@ -0,0 +1,17 @@
+import React, {Component} from './react';
+export default class About extends React.Component{
+   render(){
+       return(
+           <div>
+               <h1>Welcome to Happay</h1>
+               <h3>About Us</h3>
+               <p>Happay was founded in 2012 with a simple idea: paying someone, 
+                   be it friends, family or colleagues should be as easy as sending a text message. 
+                   With this in mind, we built our first consumer product, 
+                   the happay mobile app- a mobile wallet that allows you to pay just about anyone, anywhere,
+                   with their mobile number. No bank account details required.
+                </p>
+           </div>
+       );
+   }
+}
